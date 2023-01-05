85264a02b4c4d1e7869db72ec5b7c1c5e4119e96
neuanam16 <91107729+neuanam16@users.noreply.github.com>
2023-01-04 16:34:50 -0800

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import math
 import random
 import pygame_screen_recorder
 gconst = 9.81
+rpf = 150
+eage = True
 
 
 def supersimplify(input):
@@ -20,18 +22,15 @@ def render():
         surface = pygame.Surface((obj["size"] * 2, obj["size"] * 2), pygame.SRCALPHA)
         surface.set_colorkey((0, 0, 0))
         surface.set_alpha(128)
-        print(obj["heat"])
         pygame.draw.circle(surface, obj["heat"], (obj["size"], obj["size"]), obj["size"])
-        window.blit(surface, obj["pos"])
+        print(campos)
+        window.blit(surface, [obj["pos"][0] + campos[0], obj["pos"][1] + campos[1]])
     pygame.display.update()
 
 
 def physics():
     global objlist
     for obj in objlist:
-        if obj["fliction"]:
-            obj["speed"][0] = obj["speed"][0] * 0.9
-            obj["speed"][1] = obj["speed"][1] * 0.9
         obj["heat"][0] += (255 - obj["heat"][0])/1000
         obj["heat"][1] += (255 - obj["heat"][1])/1000
         obj["heat"][2] += (255 - obj["heat"][2])/1000
@@ -56,14 +55,16 @@ def physics():
             distance = math.sqrt(((o1minuso2x ** 2)+(o1minuso2y ** 2)))
             if distance ** 2 == 0:
                 continue
-            if distance <= obj["size"] + obj2["size"]:
+            if distance <= obj["size"] + obj2["size"] or obj["pos"][1] <= 0 or\
+                    (eage and (obj["pos"][1] >= size[1] or obj["pos"][0] <= 0 or obj["pos"][0] >= size[0])):
                 org = obj["speed"][1]
                 obj["speed"][1] = obj["speed"][1] * -1
                 obj["pos"][1] += obj["speed"][1]
                 o1minuso2x = obj["pos"][0] - obj2["pos"][0]
                 o1minuso2y = obj["pos"][1] - obj2["pos"][1]
                 distance = math.sqrt(((o1minuso2x ** 2) + (o1minuso2y ** 2)))
-                if distance <= obj["size"] + obj2["size"]:
+                if distance <= obj["size"] + obj2["size"] or obj["pos"][1] <= 0 or\
+                        (eage and (obj["pos"][1] >= size[1] or obj["pos"][0] <= 0 or obj["pos"][0] >= size[0])):
                     obj["pos"][1] -= obj["speed"][1]
                     obj["speed"][1] = org
                     org = obj["speed"][0]
@@ -76,11 +77,11 @@ def physics():
                         obj["pos"][0] -= obj["speed"][0]
                         obj["speed"][0] = org
                         while distance <= obj["size"] + obj2["size"]:
-                            obj["pos"][0] += o1minuso2x / 1000
-                            obj["pos"][1] += o1minuso2y / 1000
                             o1minuso2x = obj["pos"][0] - obj2["pos"][0]
                             o1minuso2y = obj["pos"][1] - obj2["pos"][1]
                             distance = math.sqrt(((o1minuso2x ** 2) + (o1minuso2y ** 2)))
+                            obj["pos"][0] += o1minuso2x / 1000
+                            obj["pos"][1] += o1minuso2y / 1000
                 obj["heat"][1] -= 1
                 obj["heat"][2] -= 2
                 if obj["heat"][0] > 255:
@@ -95,11 +96,15 @@ def physics():
                     obj["heat"][1] = 0
                 if obj["heat"][2] < 0:
                     obj["heat"][2] = 0
+                if obj["fliction"]:
+                    obj["speed"][0] = obj["speed"][0] * 0.9
+                    obj["speed"][1] = obj["speed"][1] * 0.9
                 continue
             obj["pos"][0] += obj["speed"][0] / len(objlist)
             obj["pos"][1] += obj["speed"][1] / len(objlist)
-            force = ((gconst * (obj["mass"]*obj2["mass"])/10) / (distance ** 2))
-            angle = math.atan2(o1minuso2y, o1minuso2x)/math.pi*180
+            force = ((gconst * ((math.pi*obj["size"]**2)*(math.pi*obj["size"]**2)*obj["mass"]*obj2["mass"])/10) /
+                     (distance ** 2))
+            angle = math.atan2(o1minuso2y, o1minuso2x)
             obj["speed"][0] += (force * (0-math.cos(angle)))
             obj["speed"][1] += (force * (0-math.sin(angle)))
 
@@ -107,12 +112,12 @@ def physics():
 def setup():
     global objlist
     for i in range(0):
-        objlist.append({"pos": [(size[0]/2)+random.randrange(-100, 100), (size[1]/2)+random.randrange(-100, 100)],
+        objlist.append({"pos": [random.randrange(10, size[0] - 10), random.randrange(10, size[1] - 10)],
                         "heat": [255, 255, 255],
                         "speed": [0, 0],
                         "size": 10,
                         "fliction": True,
-                        "mass": 10})
+                        "mass": 0.0025})
 
 
 objlist = []
@@ -124,8 +129,10 @@ setup()
 click = False
 fps = 60
 lor = 10
-recorder = pygame_screen_recorder.pygame_screen_recorder("output.mp4")
 recorded = False
+fakeinit = pygame.time.get_ticks()
+campos = [0, 0]
+pressed = []
 while running:
     window.fill((0, 0, 0))
     physics()
@@ -134,25 +141,27 @@ while running:
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-            click = True
-        if event.type == pygame.MOUSEBUTTONUP and event.button == 1:
-            click = False
-        if click:
             objlist.append(
-                {"pos": [pygame.mouse.get_pos()[0], pygame.mouse.get_pos()[1]],
+                {"pos": [pygame.mouse.get_pos()[0] - campos[0], pygame.mouse.get_pos()[1] - campos[1]],
                  "heat": [255, 255, 255],
                  "speed": [0, 0],
                  "size": lor,
                  "fliction": True,
-                 "mass": 10})
+                 "mass": 0.0025})
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_l:
-                lor = 10
-            if event.key == pygame.K_s:
-                lor = 2
-            if event.key == pygame.K_c:
+            if event.key == pygame.K_c and not recorded:
+                recorder = pygame_screen_recorder.pygame_screen_recorder("output.mp4")
                 recorded = True
-    if pygame.time.get_ticks() % 150 == 0 and recorded:
+                fakeinit = pygame.time.get_ticks()
+            if event.key == pygame.K_w:
+                campos[1] += 10
+            if event.key == pygame.K_s:
+                campos[1] -= 10
+            if event.key == pygame.K_a:
+                campos[0] += 10
+            if event.key == pygame.K_d:
+                campos[0] -= 10
+    if recorded and recorder.file_count < (pygame.time.get_ticks() - fakeinit) // (rpf*100):
         recorder.click(window)
 if recorded:
     recorder.save()
